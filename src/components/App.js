b12b9d9b3a00fb1a88393005c102ad8f19f0eb5c
Daniel Trotter <flare84@gmail.com>
2019-09-10 13:18:33 -0600

    site is up and working with the basics setup

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {
+  Route,
+  Switch,
+  Redirect,
+  Router
+} from 'react-router-dom';
+import { ScrollManager, WindowScroller } from 'react-scroll-manager';
+import { createBrowserHistory as createHistory } from 'history';
+import './App.css';
+
+import Home from './pages/Home';
+import AboutMe from './pages/AboutMe';
+
+
+class App extends Component {
+
+  componentDidMount() {
+    document.title='Lara Vanderlaan'
+  }
+
+  constructor() {
+    super();
+    this.history = createHistory();
+  }
+
+  render() {
+    return (
+      <ScrollManager history={this.history}>
+        <Router history={this.history}>
+          <WindowScroller>
+            <div id='mainAppContainer'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/about-me' component={AboutMe} />
+                <Redirect to='/' />
+              </Switch>
+            </div>
+          </WindowScroller>
+        </Router>
+      </ScrollManager>
+    );
+  }
+}
+
+export default App;
