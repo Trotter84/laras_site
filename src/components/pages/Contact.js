81d3f22746ed5ad51ebc293f87e49cb246ee63a5
Daniel Trotter <flare84@gmail.com>
2019-09-10 22:20:58 -0600

    added Instagram and Niya Models links to Contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import Styled from 'styled-components';
+
+
+const Instagram = Styled.a`
+  transition: all 200ms ease-in-out;
+  &:hover,&.active {
+    color: #7e9e9d;
+  }
+`
+
+const Niya = Styled.a`
+  transition: all 200ms ease-in-out;
+  &:hover,&.active {
+    color: #7e9e9d;
+  }
+`
 
 
 class Contact extends Component {
 
   render() {
     return(
-      <h1>Contact</h1>
+      <div id='mainContactContainer'>
+        <Instagram id='instagramLink' href='https://www.instagram.com/trippieehippy/' target='_blank'>My Instagram</Instagram>
+        <Niya id='niyaLink' href='http://niyamodels.com/page/contact/' target='_blank'>Niya Models</Niya>
+      </div>
     )
   }
 }
