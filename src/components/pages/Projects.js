cac15358bec7c8fe9c74ec22da6c5acd26fb4402
Daniel Trotter <flare84@gmail.com>
2019-09-10 21:24:31 -0600

    Projects page has two carousels now and all images are being used

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -6,6 +6,12 @@ import BikiniShot1 from '../../images/bikini_shot.jpg';
 import BikiniShot2 from '../../images/bikini_shot2.jpg';
 import BlackBikini1 from '../../images/black_bikini.jpg';
 import BlackBikini2 from '../../images/black_bikini2.jpg';
+import BlueDressInChair from '../../images/blue_dress_in_chair.jpg';
+import FluffyHoodieFront from '../../images/fluffy_hoodie_front.jpg';
+import FluffyHoodie from '../../images/fluffy_hoodie.jpg';
+import LaraSittingWithDress from '../../images/lara_sitting_with_dress.jpg';
+import YogaStretchingArms from '../../images/yoga_stretching_arms.jpg';
+import YogaStretching from '../../images/yoga_stretching.jpg';
 
 
 class Projects extends Component {
@@ -16,22 +22,46 @@ class Projects extends Component {
 
   render() {
     return(
-      <div id='projectsContainer'>
-        <div id='projectsSubContainer'>
-          <Carousel id='carouselContainer' autoPlay interval={4000} showThumbs={false} infiniteLoop>
-              <div className='frontPagePhoto1'>
-                <img src={BikiniShot1} className='frontPageImage' alt='Neon green bikini shot' />
-              </div>
-              <div className='frontPagePhoto2'>
-                <img src={BikiniShot2} className='frontPageImage' alt='Neon green bikini shot two' />
-              </div>
-              <div className='frontPagePhoto3'>
-                <img src={BlackBikini1} className='frontPageImage' alt='Black bikini shot' />
-              </div>
-              <div className='frontPagePhoto4'>
-                <img src={BlackBikini2} className='frontPageImage' alt='Black bikini shot two' />
-              </div>
-          </Carousel>
+      <div id='mainProjectsContainer'>
+        <div id='leftProjectsContainer'>
+          <div id='leftProjectsSubContainer'>
+            <Carousel id='leftCarouselContainer' autoPlay interval={5000} showThumbs={false} infiniteLoop>
+                <div className='leftPagePhoto1'>
+                  <img src={BikiniShot1} className='leftPageImage' alt='Neon green bikini shot' />
+                </div>
+                <div className='leftPagePhoto2'>
+                  <img src={BikiniShot2} className='leftPageImage' alt='Neon green bikini shot two' />
+                </div>
+                <div className='leftPagePhoto3'>
+                  <img src={BlackBikini1} className='leftPageImage' alt='Black bikini shot' />
+                </div>
+                <div className='leftPagePhoto4'>
+                  <img src={BlackBikini2} className='leftPageImage' alt='Black bikini shot two' />
+                </div>
+            </Carousel>
+          </div>
+        </div>
+
+        <div id='rightProjectsContainer'>
+          <div id='rightProjectsSubContainer'>
+            <Carousel id='rightCarouselContainer' autoPlay interval={5000} showThumbs={false} infiniteLoop>
+                <div className='rightPagePhoto1'>
+                  <img src={BlueDressInChair} className='rightPageImage' alt='Sitting in chair with blue dress' />
+                </div>
+                <div className='rightPagePhoto2'>
+                  <img src={FluffyHoodieFront} className='rightPageImage' alt='Fluffy hoodie dress on runway' />
+                </div>
+                <div className='rightPagePhoto3'>
+                  <img src={FluffyHoodie} className='rightPageImage' alt='Fluffy hoodie dress on runway shot two' />
+                </div>
+                <div className='rightPagePhoto4'>
+                  <img src={YogaStretchingArms} className='rightPageImage' alt='Doing yoga and stretching arms' />
+                </div>
+                <div className='rightPagePhoto5'>
+                  <img src={YogaStretching} className='rightPageImage' alt='Doing yoga and stretching legs' />
+                </div>
+            </Carousel>
+          </div>
         </div>
       </div>
     )
